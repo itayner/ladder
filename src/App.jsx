f92f61a5ce1d8cad465f503deb29ef111124da36
ian cho <itayner86@gmail.com>
2023-05-22 22:53:22 -0500

    two navs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 import NavMob from './components/mobile/NavMob'
+import NavDesk from './components/desktop/NavDesk'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <NavMob />
+      <NavDesk />
     </div>
   )
 }
