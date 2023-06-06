abdde132c201d8135610cd614b041a91fd9d5257
ian cho <itayner86@gmail.com>
2023-06-05 19:38:20 -0500

    msg

--- src/components/mobile/content/RegisterMob.jsx
+++ src/components/mobile/content/RegisterMob.jsx
@@ -9,6 +9,19 @@ function RegisterMob(props) {
                         <label className='form-label' for='first-name'>First Name</label>
                         <input type='text' className='form-control' id='first-name'  />
                     </div>
+                    <div className='mb-3'>
+                        <label className='form-label' for='last-name'>Last Name</label>
+                        <input type='text' className='form-control' id='last-name'  />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label' for='email-1'>Email</label>
+                        <input type='text' className='form-control' id='first-name'  />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label' for='password-1'>Create Password</label>
+                        <input type='password' className='form-control' id='password-1' />
+                        <input type='password' className='form-control' id='password-2' placeholder='Confirm password' />
+                    </div>
                 </form>
             </div>
         </div>
