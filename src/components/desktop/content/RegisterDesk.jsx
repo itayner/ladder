8f0951205eb9ca1a493049619406258b53677f9c
ian cho <itayner86@gmail.com>
2023-06-05 21:28:21 -0500

    msg

--- src/components/desktop/content/RegisterDesk.jsx
+++ src/components/desktop/content/RegisterDesk.jsx
@@ -5,14 +5,14 @@ function RegisterDesk(props) {
         <div className='reg-desk-container'>
             <div className='reg-desk-wrapper'>
             <form>
-                    <div className='mb-3'>
+                    <div className='mb-3 mt-2'>
                         <label className='form-label' for='first-name'>Name</label>
                         <input type='text' className='form-control' id='first-name' placeholder='first' />
                         <input type='text' className='form-control' id='last-name' placeholder='last' />
                     </div>
                     <div className='mb-3'>
-                        <label className='form-label' for='email-1'>Email</label>
-                        <input type='text' className='form-control' id='first-name'  />
+                        <label className='form-label' for='email'>Email</label>
+                        <input type='text' className='form-control' id='email'  />
                     </div>
                     <div className='mb-3'>
                         <label className='form-label' for='password-1'>Create Password</label>
