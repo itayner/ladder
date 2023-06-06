8876472102264a19028179f1d0db4c455149eeb4
ian cho <itayner86@gmail.com>
2023-06-05 21:08:43 -0500

    msg

--- src/components/desktop/content/RegisterDesk.jsx
+++ src/components/desktop/content/RegisterDesk.jsx
@@ -4,7 +4,23 @@ function RegisterDesk(props) {
     return (
         <div className='reg-desk-container'>
             <div className='reg-desk-wrapper'>
-
+            <form>
+                    <div className='mb-3'>
+                        <label className='form-label' for='first-name'>Name</label>
+                        <input type='text' className='form-control' id='first-name' placeholder='first' />
+                        <input type='text' className='form-control' id='last-name' placeholder='last' />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label' for='email-1'>Email</label>
+                        <input type='text' className='form-control' id='first-name'  />
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label' for='password-1'>Create Password</label>
+                        <input type='password' className='form-control' id='password-1' placeholder='password' />
+                        <input type='password' className='form-control' id='password-2' placeholder='confirm password' />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     );
--- src/components/mobile/content/RegisterMob.jsx
+++ src/components/mobile/content/RegisterMob.jsx
@@ -5,16 +5,16 @@ function RegisterMob(props) {
         <div className='reg-mob-container'>
             <div className='reg-mob-wrapper'>
                 <form>
-                    <div className='mb-3'>
+                    <div className='mb-1'>
                         <label className='form-label' for='first-name'>Name</label>
                         <input type='text' className='form-control' id='first-name' placeholder='first' />
                         <input type='text' className='form-control' id='last-name' placeholder='last' />
                     </div>
-                    <div className='mb-3'>
+                    <div className='mb-1'>
                         <label className='form-label' for='email-1'>Email</label>
                         <input type='text' className='form-control' id='first-name'  />
                     </div>
-                    <div className='mb-3'>
+                    <div className='mb-1'>
                         <label className='form-label' for='password-1'>Create Password</label>
                         <input type='password' className='form-control' id='password-1' placeholder='password' />
                         <input type='password' className='form-control' id='password-2' placeholder='confirm password' />
