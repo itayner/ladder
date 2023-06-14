6aab71881b5f64b099862d06a06770f260490fd1
ian cho <itayner86@gmail.com>
2023-06-13 21:42:39 -0500

    msg

--- src/components/desktop/content/RegisterDesk.jsx
+++ src/components/desktop/content/RegisterDesk.jsx
@@ -1,75 +1,54 @@
 import React, {useState} from 'react';
-import Joi from 'joi-browser';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function RegisterDesk(props) {
-    const [reg, setReg] = useState({
+    const initialValues = {
         first: '',
         last: '',
         email: '',
-        password: '',
-        passwordConfirmation: '',
-    })
+        password1: '',
+        password2: ''
+    };
+    const onSubmit = (values) =>{
+        console.log(values);
+        return false;
+    };
+    const validationSchema = Yup.object({
+        first: Yup.string().required(),
+        last: Yup.string().required(),
+        email: Yup.string().email().required(),
+        password1: Yup.string().required()
+        .min(3, 'Password is too short - should be 3 chars minimum.')
+        .matches(/[0-9]/, 'Password must contain #')
+        .matches(/[a-z]/, 'Password must contain lowercase letter')
+        .matches(/[A-Z]/, 'Password must contain uppercase letter'),
+        password2: Yup.string().required().oneOf([Yup.ref("password")], "Passwords do not match"),
+      });
 
-    const schema = {
-        first: Joi.string().required().min(2).label("first"),
-        last: Joi.string().required().min(2).label("last"),
-        email: Joi.string().required().email().label("email"),
-        password: Joi.string().required().min(5).label("password"),
-        passwordConfirmation: Joi.string()
-  .required()
-  .valid(Joi.ref('password'))
-  .options({
-    language: {
-      any: {
-        allowOnly: 'Passwords do not match!!!',
-      }
-    } 
-  })
-    };
-    const validate = (data, schema) => {
-        const options = {
-            abortEarly: false
-        };
-        const result = Joi.validate(data, schema, options);
-        // result.error === null -> valid
-        return result;
-    };
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log(reg);
-        const result = validate(reg, schema);
-        console.log(result);        
-    };
-    const onChange = (e) => {
-        const key = e.target.name;
-        var updated = {...reg};
-        updated[e.target.id] = e.target.value;
-        setReg(updated);
-    }
-
-    const {first, last, email, password, passwordConfirmation} = reg;
 
     return (
         <div className='reg-desk-container'>
             <div className='reg-desk-wrapper'>
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
             <form onSubmit={onSubmit}>
                     <div className='mb-3 mt-2'>
                         <label className='form-label' htmlFor='first'>Name</label>
-                        <input name='first' type='text' className='form-control' value={first} placeholder='first' onChange={onChange}/>
-                        <input name='last' type='text' className='form-control' value={last} placeholder='last' onChange={onChange}/>
+                        <Field name='first' type='text' className='form-control' placeholder='first'/>
+                        <Field name='last' type='text' className='form-control' placeholder='last'/>
                     </div>
                     <div className='mb-3'>
                         <label className='form-label' htmlFor='email'>Email</label>
-                        <input name='email' type='text' className='form-control' value={email} onChange={onChange}/>
+                        <Field name='email' type='text' className='form-control'/>
                     </div>
                     <div className='mb-3'>
                         <label className='form-label' htmlFor='password'>Create Password</label>
-                        <input name='password' type='password' className='form-control' value={password} placeholder='password' onChange={onChange}/>
-                        <input name='passwordConfirmation' type='password' className='form-control' value={passwordConfirmation} 
-                        placeholder='confirm password' onChange={onChange}/>
+                        <Field name='password1' type='password' className='form-control' placeholder='password'/>
+                        <Field name='password2' type='password' className='form-control' placeholder='confirm password'/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
+            </Formik>
             </div>
         </div>
     );
