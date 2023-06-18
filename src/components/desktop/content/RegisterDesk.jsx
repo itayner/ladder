9a6a155321e3be630111793e18b9e62c3c95b8c6
ian cho <itayner86@gmail.com>
2023-06-18 15:21:34 -0500

    msg

--- src/components/desktop/content/RegisterDesk.jsx
+++ src/components/desktop/content/RegisterDesk.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
@@ -15,23 +15,39 @@ function RegisterDesk(props) {
         return false;
     };
     const validationSchema = Yup.object({
-        first: Yup.string().required(),
-        last: Yup.string().required(),
-        email: Yup.string().email().required(),
-        password1: Yup.string().required()
-        .min(3, 'Password is too short - should be 3 chars minimum.')
-        .matches(/[0-9]/, 'Password must contain #')
-        .matches(/[a-z]/, 'Password must contain lowercase letter')
-        .matches(/[A-Z]/, 'Password must contain uppercase letter'),
-        password2: Yup.string().required().oneOf([Yup.ref("password")], "Passwords do not match"),
+        first: Yup.string().required().min(2).label("First name"),
+        last: Yup.string().required().min(2).label('Last name'),
+        email: Yup.string().email().required().label("Email"),
+        password1: Yup.string().required().min(3).label('Password'),
+        password2: Yup.string().required().oneOf([Yup.ref("password1")], "Passwords do not match").label('Password')
+        //.min(3, 'Password is too short - should be 3 chars minimum.'),
+        //.matches(/[0-9]/, 'Password must contain #')
+        //.matches(/[a-z]/, 'Password must contain lowercase letter')
+        //.matches(/[A-Z]/, 'Password must contain uppercase letter'),
+        //password2: Yup.string().required().oneOf([Yup.ref("password")], "Passwords do not match"),
       });
 
+    const getErrorElem = (errors) =>{
+        const msg = Object.values(errors).at(0);
+        return (
+            <div className='' style={{color: '#dc3545'}}>{msg}</div>
+        );
+    };
 
     return (
         <div className='reg-desk-container'>
             <div className='reg-desk-wrapper'>
-            <Formik initialValues={initialValues} onSubmit={onSubmit}>
-            <form onSubmit={onSubmit}>
+            <Formik 
+            initialValues={initialValues} 
+            onSubmit={onSubmit}
+            validationSchema={validationSchema}
+            validateOnChange={false}
+            validateOnBlur={false}
+            >
+            {
+                ({errors, touched}) => (
+            <Form>
+                {errors && getErrorElem(errors)}
                     <div className='mb-3 mt-2'>
                         <label className='form-label' htmlFor='first'>Name</label>
                         <Field name='first' type='text' className='form-control' placeholder='first'/>
@@ -46,8 +62,10 @@ function RegisterDesk(props) {
                         <Field name='password1' type='password' className='form-control' placeholder='password'/>
                         <Field name='password2' type='password' className='form-control' placeholder='confirm password'/>
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
+                    <button type="submit" onClick={()=>{console.log(errors)}} className="btn btn-primary">Submit</button>
+                </Form>
+                )
+            }
             </Formik>
             </div>
         </div>
--- src/components/mobile/content/RegisterMob.jsx
+++ src/components/mobile/content/RegisterMob.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function RegisterMob(props) {
+    const initialValues = {
+        first: '',
+        last: '',
+        email: '',
+        password1: '',
+        password2: ''
+    };
+    const onSubmit = (values) =>{
+        console.log(values);
+        return false;
+    };
+    const validationSchema = Yup.object({
+        first: Yup.string().required().min(2).label("First name"),
+        last: Yup.string().required().min(2).label('Last name'),
+        email: Yup.string().email().required().label("Email"),
+        password1: Yup.string().required().min(3).label('Password'),
+        password2: Yup.string().required().oneOf([Yup.ref("password1")], "Passwords do not match").label('Password')
+        //.min(3, 'Password is too short - should be 3 chars minimum.'),
+        //.matches(/[0-9]/, 'Password must contain #')
+        //.matches(/[a-z]/, 'Password must contain lowercase letter')
+        //.matches(/[A-Z]/, 'Password must contain uppercase letter'),
+        //password2: Yup.string().required().oneOf([Yup.ref("password")], "Passwords do not match"),
+      });
+
+    const getErrorElem = (errors) =>{
+        const msg = Object.values(errors).at(0);
+        return (
+            <div className='' style={{color: '#dc3545'}}>{msg}</div>
+        );
+    };
+
     return (
         <div className='reg-mob-container'>
             <div className='reg-mob-wrapper'>
+                {
+                    /*
                 <form>
                     <div className='mb-1'>
                         <label className='form-label' htmlFor='first'>Name</label>
@@ -22,6 +56,38 @@ function RegisterMob(props) {
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
+                    */
+                }
+                <Formik 
+            initialValues={initialValues} 
+            onSubmit={onSubmit}
+            validationSchema={validationSchema}
+            validateOnChange={false}
+            validateOnBlur={false}
+            >
+            {
+                ({errors, touched}) => (
+            <Form>
+                {errors && getErrorElem(errors)}
+                    <div className='mb-1'>
+                        <label className='form-label' htmlFor='first'>Name</label>
+                        <Field name='first' type='text' className='form-control' placeholder='first'/>
+                        <Field name='last' type='text' className='form-control' placeholder='last'/>
+                    </div>
+                    <div className='mb-1'>
+                        <label className='form-label' htmlFor='email'>Email</label>
+                        <Field name='email' type='text' className='form-control'/>
+                    </div>
+                    <div className='mb-1'>
+                        <label className='form-label' htmlFor='password'>Create Password</label>
+                        <Field name='password1' type='password' className='form-control' placeholder='password'/>
+                        <Field name='password2' type='password' className='form-control' placeholder='confirm password'/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </Form>
+                )
+            }
+            </Formik>
             </div>
         </div>
     );
